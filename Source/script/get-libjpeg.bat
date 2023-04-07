5f02d99d988c02e5a38ff92a974117699fb8363a
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2023-04-07 19:27:33 +0300

    ...

---
+++ Source/script/get-libjpeg.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+:: This batch script downloads the 'libjpeg' library.
+:: This script must be executed from the 'script' folder.
+
+:: Settings.
+SET Repository=https://github.com/vault-thirteen/libjpeg.git
+SET LibFolder=libjpeg
+SET LibArch=x64
+SET LibVersion=9e
+SET ScriptFolder=script
+
+:: Download the Code.
+git clone %Repository%
+
+:: Prepare the code.
+CD ..
+MKDIR "%LibFolder%"
+MKDIR "%LibFolder%\%LibArch%"
+MKDIR "%LibFolder%\%LibArch%\include"
+MKDIR "%LibFolder%\%LibArch%\include\%LibFolder%"
+
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg\*.obj" "%LibFolder%\%LibArch%\"
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg.lib" "%LibFolder%\%LibArch%\"
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg.pdb" "%LibFolder%\%LibArch%\"
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\include\*.h" "%LibFolder%\%LibArch%\include\%LibFolder%\"
+RMDIR /Q /S "%ScriptFolder%\%LibFolder%"
+
+CD "%ScriptFolder%"
---
+++ Source/script/get-libpng.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+:: This batch script downloads the 'libpng' library.
+:: This script must be executed from the 'script' folder.
+
+:: Settings.
+SET Repository=https://github.com/vault-thirteen/libpng.git
+SET LibFolder=libpng
+SET LibVersion=1.6.39
+SET ScriptFolder=script
+
+:: Download the Code.
+git clone %Repository%
+
+:: Prepare the code.
+CD ..
+MKDIR "%LibFolder%"
+MKDIR "%LibFolder%\bin"
+MKDIR "%LibFolder%\include"
+MKDIR "%LibFolder%\include\%LibFolder%"
+MKDIR "%LibFolder%\lib"
+
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\libpng*.dll" "%LibFolder%\bin\"
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\libpng*.lib" "%LibFolder%\lib\"
+MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\include\*.h" "%LibFolder%\include\%LibFolder%\"
+RMDIR /Q /S "%ScriptFolder%\%LibFolder%"
+
+CD "%ScriptFolder%"
