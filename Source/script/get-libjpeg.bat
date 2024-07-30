be68c10d9188b6c45a76443c5d4da03ec450d0af
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2024-07-30 21:11:05 +0300

    Script for 'libjpeg'.

--- Source/script/get-libjpeg.bat
+++ Source/script/get-libjpeg.bat
@@ -7,7 +7,7 @@
 SET Repository=https://github.com/vault-thirteen/libjpeg.git
 SET LibFolder=libjpeg
 SET LibArch=x64
-SET LibVersion=9e
+SET LibVersion=9f
 SET ScriptFolder=script
 
 :: Download the Code.
@@ -20,7 +20,6 @@ MKDIR "%LibFolder%\%LibArch%"
 MKDIR "%LibFolder%\%LibArch%\include"
 MKDIR "%LibFolder%\%LibArch%\include\%LibFolder%"
 
-MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg\*.obj" "%LibFolder%\%LibArch%\"
 MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg.lib" "%LibFolder%\%LibArch%\"
 MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\%LibArch%\jpeg.pdb" "%LibFolder%\%LibArch%\"
 MOVE "%ScriptFolder%\%LibFolder%\Build\%LibVersion%\include\*.h" "%LibFolder%\%LibArch%\include\%LibFolder%\"
